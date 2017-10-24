cb0274405d31c25dd6551a9977f498a615588f83
Alexander Baas <ajmbaas@outlook.com>
2017-10-24 11:09:31 +0200

    Add admins controller and if.admin?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
+  def after_sign_in_path_for(current_user)
+    # http://www.rubydoc.info/github/plataformatec/devise/Devise/Controllers/Helpers:after_sign_in_path_for
+    if current_user.admin?
+        admin_path(current_user)
+    else
+        student_path(current_user)
+    end
+  end
 end
