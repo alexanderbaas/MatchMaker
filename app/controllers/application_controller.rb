3310bb01b18ce5a7f42b9991607dba5bd54b31b9
Alexander Baas <ajmbaas@outlook.com>
2017-10-26 11:21:00 +0200

    Devise add name field to sign up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-def authorize_admin
-    redirect_to student_path(current_user) unless current_user.admin
-end
+    def authorize_admin
+        redirect_to student_path(current_user) unless current_user.admin
+    end
 
   def after_sign_in_path_for(current_user)
   #  http://www.rubydoc.info/github/plataformatec/devise/Devise/Controllers/Helpers:after_sign_in_path_for
@@ -15,6 +16,14 @@ end
 
     end
   end
-end
 # error: https://www.thoughtco.com/nameerror-uninitialized-2907928
 # https://github.com/bitmakerlabs/debugging-guide/wiki/Rails-error:-Routing-Error:-uninitialized-constant
+
+
+protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,6 +8,16 @@ class StudentsController < ApplicationController
     end
 
 
+  def meth
+    @students = User.all.where(admin: false)
+        @students = @students.select(:email).map(&:attributes)
+        @student = @students.shift
+        @students.shuffle!
 
-    
+        @students.length.times do |index_number|
+        puts "day #{index_number + 1} #{student} matches with #{students.first}"
+        @students.rotate
+      end
+    end
+
 end
